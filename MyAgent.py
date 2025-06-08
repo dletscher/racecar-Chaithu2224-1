3d276ad732ef5c20d4e36561b5f1b4199968bf36
Chaithu2224 <saichaithanyateja.salkapuram@slu.edu>
2025-06-08 17:39:33 -0500

    My Agent file

---
+++ MyAgent.py
@@ -0,0 +1,35 @@
+import random
+
+
+class Agent:
+    def chooseAction(self, observations, possibleActions):
+        lidar = observations['lidar']  
+        velocity = observations['velocity']
+
+
+        l10 = lidar[1]
+        center = lidar[2]
+        r10 = lidar[3]
+        avg=(l10+r10)/2
+
+        if r10 < avg and r10>0.2:
+            direction = 'left'
+        elif l10 < avg and l10>0.2:
+            direction = 'right'
+        else:
+            direction = 'straight'
+    
+        if velocity==0:
+            speed='accelerate'
+        if velocity < 0.2 and center >0.4:
+            speed = 'accelerate'
+        elif (lidar[0]<0.4 or lidar[4]<0.4) or velocity>0.6:
+            speed = 'brake'
+        else:
+            speed = 'coast'
+
+        action = (direction, speed)
+        return action
+
+
+
